442a57e9bfa2eaa0e7ce23eac940cbca9c65e665
Nicole Rowley <rowleyn@rcn.org.uk>
2021-12-06 14:44:22 +0000

    Fix error with _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { Layout } from "../components/Layout";
 import "../styles/globals.css";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
