8d2b1ed8482c446d0df0f99fbbe88565d5c8d5b1
Nicole Rowley <rowleyn@rcn.org.uk>
2021-12-06 17:32:15 +0000

    try 25000 fix to CORS

--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -4,6 +4,9 @@ import { ResponseFuncs } from "../../../utils/types";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
+  if (method === "OPTIONS") {
+    return res.status(200).send("ok");
+  }
   const catcher = (error: Error) => res.status(400).json({ error });
 
   const handleCase: ResponseFuncs = {
@@ -19,7 +22,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const response = handleCase[method];
   if (response) {
-    res.status(200).end;
     response(req, res);
   } else res.status(400).json({ error: "No Response for This Request" });
 };
